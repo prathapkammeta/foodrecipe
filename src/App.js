774cbddfa5c1244edeb575d17e225dc878e09ac9
prathapkammeta905 <prathapkammeta905@gmail.com>
2022-03-15 21:30:45 +0530

    hift

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react'
+import SearchMeal from './components/SearchMeal';
+import RecipeCard from './components/RecipeCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+const apiUrl="https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+const App = () => {
+
+
+
+
+
+ const[isLoading,setIsLoading]=useState(false);
+ const[query,setQuery]=useState("");
+ const[recipes,setRecipes]=useState([]);
+//  function to search for the meals
+
+
+  const searchRecipes=async ()=>{
+    setIsLoading(true);
+    const url=apiUrl+query;
+    const res=await fetch(url);
+    const data=await res.json();
+    console.log(data);
+    setRecipes(data.meals);
+    setIsLoading(false);
+    setQuery("");
+  };
+  useEffect(()=>{
+    searchRecipes();
+     
+  },[])
+
+const handleSubmit=event=>{
+  event.preventDefault();
+  searchRecipes();
 }
 
-export default App;
+  return (
+    <div className="container">
+      <h1>Recipe Application</h1>
+      <SearchMeal
+      handleSubmit={handleSubmit}
+      value={query}
+      onChange={event=>setQuery(event.target.value)}
+      isLoading={isLoading}
+      />
+      <div className="recipes">
+      
+      {recipes ? recipes.map(recipe=>(
+        <RecipeCard
+        key={recipe.idMeal}
+        recipe={recipe}
+
+        
+        />
+      )):"No Recipes!"}
+             </div>
+      </div>
+  )
+
+}
+
+
+export default App
+hi
+
